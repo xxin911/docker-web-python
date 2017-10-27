f5ab65dbbc90543ab549afa26fecc894ce149c8f
simpson <code-xx@foxmail.com>
2017-10-27 09:54:24 +0800

    弹幕首页

--- app/manage.py
+++ app/manage.py
@@ -4,13 +4,13 @@
 # @Site    : 
 # @File    : manage.py
 # @Software: PyCharm
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
